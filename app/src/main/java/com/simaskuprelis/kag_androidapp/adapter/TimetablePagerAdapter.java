f29769a9893581dc536ae922ee4531985b194a30
Simas Kuprelis <simas.kuprelis@gmail.com>
2017-05-11 21:39:58 +0300

    add timetable fragment classes

---
+++ app/src/main/java/com/simaskuprelis/kag_androidapp/adapter/TimetablePagerAdapter.java
@@ -0,0 +1,22 @@
+package com.simaskuprelis.kag_androidapp.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+public class TimetablePagerAdapter extends FragmentStatePagerAdapter {
+
+    public TimetablePagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+}
--- app/src/main/java/com/simaskuprelis/kag_androidapp/fragment/TimetableFragment.java
+++ app/src/main/java/com/simaskuprelis/kag_androidapp/fragment/TimetableFragment.java
@@ -1,41 +1,45 @@
 package com.simaskuprelis.kag_androidapp.fragment;
 
-
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
-import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.simaskuprelis.kag_androidapp.R;
 
-import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class TimetablePagerFragment extends Fragment {
 
-    @BindView(R.id.pager)
-    ViewPager mPager;
-    @BindView(R.id.pager_tabs)
-    TabLayout mTabs;
-    @BindView(R.id.edit_fab)
-    FloatingActionButton mEditFab;
+public class TimetableFragment extends Fragment {
+
+    private static final String KEY_DAY = "day";
+
+    private int mDay;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Bundle args = getArguments();
+        mDay = args.getInt(KEY_DAY);
     }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_timetable_pager, container, false);
+        View v = inflater.inflate(R.layout.fragment_timetable, container, false);
         ButterKnife.bind(this, v);
 
         return v;
+    }
+
+    public static TimetableFragment newInstance(int day) {
+        Bundle args = new Bundle();
+        args.putInt(KEY_DAY, day);
+        TimetableFragment fragment = new TimetableFragment();
+        fragment.setArguments(args);
+        return fragment;
     }
 }
