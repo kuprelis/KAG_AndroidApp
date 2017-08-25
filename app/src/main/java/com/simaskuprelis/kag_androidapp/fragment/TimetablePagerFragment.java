428db696bc11ddbde798723e0bd8f0ce7228686b
Simas Kuprelis <simas.kuprelis@gmail.com>
2017-08-25 12:46:02 +0300

    add back navigation for timetable

--- app/src/main/java/com/simaskuprelis/kag_androidapp/fragment/TimetablePagerFragment.java
+++ app/src/main/java/com/simaskuprelis/kag_androidapp/fragment/TimetablePagerFragment.java
@@ -10,6 +10,7 @@ import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.util.Pair;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -33,6 +34,7 @@ import org.greenrobot.eventbus.Subscribe;
 
 import java.util.Calendar;
 import java.util.List;
+import java.util.Stack;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -50,9 +52,12 @@ public class TimetablePagerFragment extends Fragment {
 
     private List<Group> mGroups;
     private List<Integer> mTimes;
-    private String mDefaultId;
-    private String mDefaultName;
     private int mPage;
+    private int mCallbacksReceived;
+
+    // Pair structure: {id, name}
+    private Pair<String, String> mCurrent;
+    private Stack<Pair<String, String>> mHistory;
 
 
     @Override
@@ -60,10 +65,12 @@ public class TimetablePagerFragment extends Fragment {
         super.onCreate(savedInstanceState);
 
         setHasOptionsMenu(true);
+        mCallbacksReceived = 0;
 
         FirebaseDatabaseApi.getTimes(new FirebaseListener<List<Integer>>() {
             @Override
             public void onLoad(List<Integer> list) {
+                mCallbacksReceived++;
                 mTimes = list;
                 setupAdapter();
             }
@@ -73,12 +80,10 @@ public class TimetablePagerFragment extends Fragment {
             }
         });
 
+        mHistory = new Stack<>();
         mPage = getTodayIndex();
 
-        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(getContext());
-        mDefaultId = sp.getString(getString(R.string.pref_user_id), null);
-        mDefaultName = sp.getString(getString(R.string.pref_user_name), null);
-        loadData(mDefaultId, mDefaultName);
+        setNode(getDefaultNode(), false);
     }
 
     @Nullable
@@ -117,11 +122,15 @@ public class TimetablePagerFragment extends Fragment {
             case R.id.menu_pick_node:
                 Intent i = new Intent(getContext(), NodePickActivity.class);
                 startActivityForResult(i, REQUEST_NODE_ID);
-                break;
+                return true;
 
             case R.id.menu_default_node:
-                loadData(mDefaultId, mDefaultName);
-                break;
+                setNode(getDefaultNode(), true);
+                return true;
+
+            case R.id.menu_back:
+                if (!mHistory.empty()) setNode(mHistory.pop(), false);
+                return true;
         }
 
         return super.onOptionsItemSelected(item);
@@ -134,10 +143,11 @@ public class TimetablePagerFragment extends Fragment {
 
         if (requestCode == REQUEST_NODE_ID) {
             Node n = data.getParcelableExtra(NodePickActivity.RESULT_NODE);
-            loadData(n.getId(), n.getName());
+            setNode(new Pair<>(n.getId(), n.getName()), true);
+
         } else if (requestCode == REQUEST_GROUP_NODE_ID) {
             Node n = data.getParcelableExtra(GroupActivity.RESULT_GROUP_NODE);
-            loadData(n.getId(), n.getName());
+            setNode(new Pair<>(n.getId(), n.getName()), true);
         }
     }
 
@@ -155,18 +165,24 @@ public class TimetablePagerFragment extends Fragment {
         startActivityForResult(i, REQUEST_GROUP_NODE_ID);
     }
 
-    private void loadData(String id, String name) {
+    private void setNode(Pair<String, String> node, boolean saveCurrent) {
+        if (mCurrent != null && mCurrent.equals(node)) return;
+
         if (mPager != null) {
             mPage = mPager.getCurrentItem();
             mPager.setAdapter(null);
             mLoading.setVisibility(View.VISIBLE);
         }
 
-        getActivity().setTitle(name);
+        if (saveCurrent && mCurrent != null) mHistory.add(mCurrent);
+        mCurrent = node;
 
-        FirebaseDatabaseApi.getNodeGroups(id, new FirebaseListener<List<Group>>() {
+        getActivity().setTitle(node.second);
+
+        FirebaseDatabaseApi.getNodeGroups(node.first, new FirebaseListener<List<Group>>() {
             @Override
             public void onLoad(List<Group> list) {
+                mCallbacksReceived++;
                 mGroups = list;
                 setupAdapter();
             }
@@ -178,7 +194,7 @@ public class TimetablePagerFragment extends Fragment {
     }
 
     private void setupAdapter() {
-        if (getActivity() == null || mGroups == null || mTimes == null || mPager == null) return;
+        if (getActivity() == null || mPager == null || mCallbacksReceived < 2) return;
 
         mLoading.setVisibility(View.GONE);
         FragmentManager fm = getActivity().getSupportFragmentManager();
@@ -195,6 +211,13 @@ public class TimetablePagerFragment extends Fragment {
             case Calendar.FRIDAY: return 4;
             default: return 0;
         }
+    }
+
+    private Pair<String, String> getDefaultNode() {
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(getContext());
+        String id = sp.getString(getString(R.string.pref_user_id), null);
+        String name = sp.getString(getString(R.string.pref_user_name), null);
+        return new Pair<>(id, name);
     }
 
     public void reset() {
